c81c9246868e3d7bb3cd880e21bb425bca17d111
luiz-davi <luizd4398@gmail.com>
2023-03-30 19:16:18 -0300

    configurando swagger da listagem de ambientes publicos

--- src/routes/general.route.ts
+++ src/routes/general.route.ts
@@ -3,6 +3,6 @@ import { Router }  from 'express';
 
 const routes = Router();
 
-routes.get('/', GeneralController.environsments);
+routes.get('/public/environments', GeneralController.environsments);
 
 export default routes;
